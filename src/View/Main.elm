c2b327e82aa1c8db5d64e0b224dbe5194b49f51e
James Carlson <jxxcarlson@gmail.com>
2024-06-23 11:16:12 +0100

    refactor (move)

--- src/View/Main.elm
+++ src/View/Main.elm
@@ -2,12 +2,13 @@ module View.Main exposing (view)
 
 import Browser
 import Element exposing (Element)
+import Element.Background
 import Element.Font
 import Pages.Counter
 import Pages.Home
-import Pages.Parts
 import Route exposing (Route(..))
 import Types exposing (FrontendModel(..), FrontendMsg, LoadedModel)
+import View.Color
 
 
 noFocus : Element.FocusStyle
@@ -46,7 +47,74 @@ loadedView : LoadedModel -> Element FrontendMsg
 loadedView model =
     case model.route of
         HomepageRoute ->
-            Pages.Parts.generic model Pages.Home.view
+            generic model Pages.Home.view
 
         CounterPageRoute ->
-            Pages.Parts.generic model Pages.Counter.view
+            generic model Pages.Counter.view
+
+
+generic : Types.LoadedModel -> (Types.LoadedModel -> Element Types.FrontendMsg) -> Element Types.FrontendMsg
+generic model view_ =
+    Element.column
+        [ Element.width Element.fill, Element.height Element.fill ]
+        [ header model model.route { window = model.window, isCompact = True }
+        , Element.column
+            (Element.padding 20
+                :: Element.scrollbarY
+                :: Element.height (Element.px <| model.window.height - 95)
+                :: []
+            )
+            [ view_ model
+            ]
+        , footer model.route model
+        ]
+
+
+header : Types.LoadedModel -> Route -> { window : { width : Int, height : Int }, isCompact : Bool } -> Element Types.FrontendMsg
+header model route config =
+    Element.el
+        [ Element.Background.color View.Color.blue
+        , Element.paddingXY 24 16
+        , Element.width (Element.px config.window.width)
+        , Element.alignTop
+        ]
+        (Element.wrappedRow
+            [ Element.spacing 24
+            , Element.Background.color View.Color.blue
+            , Element.Font.color (Element.rgb 1 1 1)
+            ]
+            [ Element.link
+                (linkStyle route HomepageRoute)
+                { url = Route.encode HomepageRoute, label = Element.text "Home" }
+            , Element.link
+                (linkStyle route CounterPageRoute)
+                { url = Route.encode CounterPageRoute, label = Element.text "Counter" }
+            ]
+        )
+
+
+linkStyle : Route -> Route -> List (Element.Attribute msg)
+linkStyle currentRoute route =
+    if currentRoute == route then
+        [ Element.Font.underline, Element.Font.color View.Color.yellow ]
+
+    else
+        [ Element.Font.color View.Color.white ]
+
+
+footer : Route -> Types.LoadedModel -> Element msg
+footer route model =
+    Element.el
+        [ Element.Background.color View.Color.blue
+        , Element.paddingXY 24 16
+        , Element.width Element.fill
+        , Element.alignBottom
+        ]
+        (Element.wrappedRow
+            [ Element.spacing 32
+            , Element.Background.color View.Color.blue
+            , Element.Font.color (Element.rgb 1 1 1)
+            ]
+            [ Element.el [ Element.Font.color (Element.rgb 1 1 1) ] (Element.text model.message)
+            ]
+        )

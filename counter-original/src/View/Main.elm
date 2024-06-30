5a7602d77d85742b6a72a0c0ffb29493a5de8965
James Carlson <jxxcarlson@gmail.com>
2024-06-30 14:42:22 -0400

    fix headerRow function

--- counter-original/src/View/Main.elm
+++ counter-original/src/View/Main.elm
@@ -57,7 +57,8 @@ generic : Types.LoadedModel -> (Types.LoadedModel -> Element Types.FrontendMsg) 
 generic model view_ =
     Element.column
         [ Element.width Element.fill, Element.height Element.fill ]
-        [ headerView model model.route { window = model.window, isCompact = True }
+        [ Element.row [ Element.width (Element.px model.window.width), Element.Background.color View.Color.blue ]
+            (headerRow model)
         , Element.column
             (Element.padding 20
                 :: Element.scrollbarY
@@ -70,6 +71,9 @@ generic model view_ =
         ]
 
 
+headerRow model =
+    [ headerView model model.route { window = model.window, isCompact = True } ]
+    
 headerView : Types.LoadedModel -> Route -> { window : { width : Int, height : Int }, isCompact : Bool } -> Element Types.FrontendMsg
 headerView model route config =
     Element.el
